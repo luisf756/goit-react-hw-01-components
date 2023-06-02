6c1500215638ad5474ea54768957b460fd93daf7
luisf756 <luchof756@gmail.com>
2023-06-01 20:28:44 -0500

    add data in Statistics

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,25 +1,34 @@
 import style from './Statistics.module.css'
 export const Statistics = ({stats}) => {
+  const primero=stats[0];
+  const seg=stats[1];
+  const ter=stats[2];
+  const cuar=stats[3];
+  const quin=stats[4];
     return (
         <section className={style.statistics}>
         <h2 className={style.title}>Upload stats</h2>
       
         <ul className={style.statlist}>
-          <li className={style.item} key={stats.id="id-1"} style={{ backgroundColor: '#178AE4' }}>
-            <span className={style.label}> {stats.label=".docx"}</span>
-            <span className={style.percentage}>4%</span>
+          <li className={style.item} key={primero.id} style={{ backgroundColor: '#178AE4' }}>
+            <span className={style.label}> {primero.label}</span>
+            <span className={style.percentage}>{primero.percentage}%</span>
           </li>
-          <li className={style.item} key={stats.id="id-2"} style={{ backgroundColor: '#B31CD1' }}>
-            <span className={style.label}>.mp3</span>
-            <span className={style.percentage}>14%</span>
+          <li className={style.item} key={seg.id} style={{ backgroundColor: '#B31CD1' }}>
+            <span className={style.label}>{seg.label}</span>
+            <span className={style.percentage}>{seg.percentage}%</span>
           </li>
-          <li className={style.item} key={stats.id="id-3"} style={{ backgroundColor: '#B01D1D' }}>
-            <span className={style.label}>.pdf</span>
-            <span className={style.percentage}>41%</span>
+          <li className={style.item} key={ter.id} style={{ backgroundColor: '#B01D1D' }}>
+            <span className={style.label}>{ter.label}</span>
+            <span className={style.percentage}>{ter.percentage}%</span>
           </li>
-          <li className={style.item} key={stats.id="id-4"} style={{ backgroundColor: '#1FC5D8' }}>
-            <span className={style.label}>.mp4</span>
-            <span className={style.percentage}>12%</span>
+          <li className={style.item} key={cuar.id} style={{ backgroundColor: '#1FC5D8' }}>
+            <span className={style.label}>{cuar.label}</span>
+            <span className={style.percentage}>{cuar.percentage}%</span>
+          </li>
+          <li className={style.item} key={quin.id} style={{ backgroundColor: '#1FD84C' }}>
+            <span className={style.label}>{quin.label}</span>
+            <span className={style.percentage}>{quin.percentage}%</span>
           </li>
         </ul>
       </section>
---
+++ src/components/transactionHistory/prueba.js
@@ -0,0 +1,15 @@
+data=[
+    { "id": "id-1", "label": ".docx", "percentage": 22 },
+    { "id": "id-2", "label": ".pdf", "percentage": 4 },
+    { "id": "id-3", "label": ".mp3", "percentage": 17 },
+    { "id": "id-4", "label": ".psd", "percentage": 47 },
+    { "id": "id-5", "label": ".pdf", "percentage": 10 }
+  ];
+//   console.log(data);
+  
+    // const statis=friends.map(friend => ());
+  const stat1 = data
+      .filter((id,label,percentage)=> id == id-1)
+    //   console.log(stat1.id)
+  const primero=data[0]
+  console.log(primero.id)
